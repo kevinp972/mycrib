56412df480353d34ec8b4453544bcb0ae4c89aca
kevinp972 <pengxs972@gmail.com>
2025-06-17 01:13:11 -0700

    Changed Font Sizes and Picture - Finished First Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,26 +12,32 @@ export default function Home() {
               Hi, I'm Xueshan (Kevin) Peng — a data scientist graduating in December 2025, seeking full-time roles starting in January 2026.
             </p>
 
-            <div className="grid grid-cols-2 gap-8 mb-12">
+            <div className="grid grid-cols-2 gap-8 mb-12 pt-10">
               <div>
                 <h2 className="text-2xl font-medium mb-2">Currently</h2>
-                <p className="text-base font-light">UCLA MSBA Candidate <br /> DS @ JBREC</p>
+                <p className="text-lg font-extralight">
+                  <a href="https://www.anderson.ucla.edu/degrees/master-of-science-in-business-analytics-msba" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-600">UCLA MSBA</a> Candidate 
+                  <br />
+                  DS Intern @ <a href="https://jbrec.com/" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-600">JBREC</a>
+                  <br />
+                  DA Intern @ <a href="https://www.anderson.ucla.edu/about/centers/impactanderson" target="_blank" rel="noopener noreferrer" className="underline hover:text-gray-600">Center for Impact</a>
+                </p>
               </div>
               <div>
                 <h2 className="text-2xl font-medium mb-2">Location</h2>
-                <p className="text-base font-light">Los Angeles, CA</p>
+                <p className="text-lg font-extralight">Los Angeles, CA</p>
               </div>
             </div>
 
             <Link 
               href="/about"
-              className="inline-block bg-[#2D2D2D] text-white px-6 py-3 rounded-full text-lg hover:bg-black transition-colors"
+              className="inline-block bg-[#2D2D2D] text-white px-7 py-3 rounded-full text-xl hover:bg-black transition-colors"
             >
               About me
             </Link>
           </div>
 
-          <div className="relative w-[400px] h-[500px] rounded-[40px] overflow-hidden">
+          <div className="relative w-[400px] h-[400px] overflow-hidden">
             <Image
               src={profilePic}
               alt="Kevin Peng"
