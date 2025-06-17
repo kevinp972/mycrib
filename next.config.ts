41cd726551013c2a791d32b3e5d885f2522d2848
kevinp972 <pengxs972@gmail.com>
2025-06-17 16:55:17 -0700

    Add Export in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     disableStaticImages: false,
-  }
+  },
+  output: 'export',
 };
 
 export default nextConfig;
