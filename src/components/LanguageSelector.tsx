b0f776d7fffe52a9dbc09fa55e52585984e58854
kevinp972 <pengxs972@gmail.com>
2025-06-17 15:09:32 -0700

    Update LanguageSelector.tsx

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -11,22 +11,34 @@ import {
 import { ChevronDown } from 'lucide-react';
 
 export function LanguageSelector() {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="p-0 font-normal hover:bg-transparent text-xl">
-          Language
-          <ChevronDown className="h-4 w-4 ml-1" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="start" className="w-[120px] bg-[#D1FFB7]">
-        <DropdownMenuItem className="cursor-pointer focus:bg-[#bef299] text-xl">
-          English
-        </DropdownMenuItem>
-        <DropdownMenuItem className="cursor-pointer focus:bg-[#bef299] text-xl">
-          中文 - Coming Soon
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div 
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      className="relative flex items-center"
+    >
+      <DropdownMenu open={open} onOpenChange={setOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="p-0 font-normal hover:bg-transparent text-xl">
+            Language
+            <ChevronDown className="h-4 w-4 ml-1" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent 
+          align="start" 
+          className="flex bg-[#2D2D2D] text-white rounded-none mt-0 border-0 shadow-none"
+          sideOffset={0}
+        >
+          <DropdownMenuItem className="cursor-pointer hover:bg-black text-lg rounded-none px-3 py-2">
+            English
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer hover:bg-black text-lg rounded-none px-3 py-2 whitespace-nowrap">
+            中文 - Coming Soon
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 } 
