06e333c0bfc0739b2bf65e43518f12dc193ee868
kevinp972 <pengxs972@gmail.com>
2025-06-18 12:57:40 -0700

    Hide Language Selector Below 420px

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,14 +8,16 @@ export default function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="w-full bg-[#f8f4ec] px-8 xs:py-20 md:py-30">
+    <nav className="w-full bg-[#f8f4ec] px-8 py-20 md:py-30">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row md:justify-between md:items-center gap-8">
         <div className="flex items-center gap-4 justify-center md:justify-start">
           <Link href="/" className="text-2xl font-semibold text-[#183c34] hover:text-[#eaec90] transition-colors text-right">
             XUESHAN PENG
           </Link>
-          <div className="h-20 w-[1px] bg-black"></div>
-          <LanguageSelector />
+          <div className="h-20 w-[1px] bg-black hidden min-[420px]:block"></div>
+          <div className="hidden min-[420px]:block">
+            <LanguageSelector />
+          </div>
         </div>
         
         <div className="flex items-center justify-center md:justify-start space-x-13">
