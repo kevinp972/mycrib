a276571acb47906e2508d79641d38cd00235744c
kevinp972 <pengxs972@gmail.com>
2025-06-18 17:40:55 -0700

    Nav Bar - justify-start

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,7 +10,7 @@ export default function Navigation() {
   return (
     <nav className="w-full bg-[#f8f4ec] px-8 py-20 md:py-30">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row md:justify-between md:items-center gap-8">
-        <div className="flex items-center gap-4 justify-center md:justify-start">
+        <div className="flex items-center gap-4 min-[420px]:justify-center md:justify-start">
           <Link href="/" className="text-2xl font-semibold text-[#183c34] hover:text-[#eaec90] transition-colors text-right">
             XUESHAN PENG
           </Link>
@@ -20,7 +20,7 @@ export default function Navigation() {
           </div>
         </div>
         
-        <div className="flex items-center justify-center md:justify-start space-x-13">
+        <div className="grid grid-rows-2 min-[460px]:flex min-[460px]:items-center min-[460px]:justify-center min-[460px]:space-x-13">
           <Link
             href="/about"
             className={`text-xl ${pathname === '/about' ? 'text-black' : 'text-gray-600'}`}
