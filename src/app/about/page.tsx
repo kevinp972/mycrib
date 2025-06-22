6e53c99ab26390eace3fe7498c0c94dad968cc84
kevinp972 <pengxs972@gmail.com>
2025-06-21 18:56:24 -0700

    About Page First Iteration

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,5 @@
+import AboutContent from "@/components/AboutContent"
+
 export default function AboutPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-semibold">About Page - Coming Soon</h1>
-    </div>
-  );
+  return <AboutContent />
 } 
